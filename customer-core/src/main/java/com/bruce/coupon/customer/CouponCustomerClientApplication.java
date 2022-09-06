e1cc662eda8b5a7e3278569f1260babb54933a43
上官镜文 <shangguanjingwen@zj.tech>
2022-09-06 10:38:26 +0800

    构建customer项目

--- customer-core/src/main/java/com/bruce/coupon/customer/CouponCustomerClientApplication.java
+++ customer-core/src/main/java/com/bruce/coupon/customer/CouponCustomerClientApplication.java
@@ -1,10 +1,12 @@
 package com.bruce.coupon.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bruce.coupon.customer.*"})
+@ComponentScan(basePackages = {"com.bruce"})
+@EnableDiscoveryClient
 public class CouponCustomerClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(CouponCustomerClientApplication.class, args);
