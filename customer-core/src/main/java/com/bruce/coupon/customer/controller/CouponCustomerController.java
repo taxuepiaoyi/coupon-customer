90d84bbb33fb05143e92c6f5feaaa67fc9b40a1c
上官镜文 <shangguanjingwen@zj.tech>
2023-05-13 15:46:18 +0800

    引入nacos config配置动态更新

---
+++ customer-core/src/main/java/com/bruce/coupon/customer/controller/CouponCustomerController.java
@@ -0,0 +1,66 @@
+package com.bruce.coupon.customer.controller;
+
+import com.bruce.coupon.calculation.domain.ShoppingCart;
+import com.bruce.coupon.calculation.domain.SimulationOrder;
+import com.bruce.coupon.calculation.domain.SimulationResponse;
+import com.bruce.coupon.customer.domain.CouponDTO;
+import com.bruce.coupon.customer.domain.RequestCoupon;
+import com.bruce.coupon.customer.domain.SearchCoupon;
+import com.bruce.coupon.customer.service.CouponCustomerService;
+import com.bruce.coupon.template.domain.CouponInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("coupon-customer")
+public class CouponCustomerController {
+
+    @Value("${disableCouponRequest:false}")
+    private Boolean disableCoupon;
+
+    @Autowired
+    private CouponCustomerService customerService;
+
+    @PostMapping("requestCoupon")
+    public CouponDTO requestCoupon(@Valid @RequestBody RequestCoupon request) {
+        if(!disableCoupon){
+            log.info("requestCoupon.......disableCoupon is {}.........",disableCoupon);
+            return null ;
+        }
+        return customerService.requestCoupon(request);
+    }
+
+    // 用户删除优惠券
+    @DeleteMapping("deleteCoupon")
+    public void deleteCoupon(@RequestParam("userId") Long userId,
+                             @RequestParam("couponId") Long couponId) {
+        customerService.deleteCoupon(userId, couponId);
+    }
+
+    // 用户模拟计算每个优惠券的优惠价格
+    @PostMapping("simulateOrder")
+    public SimulationResponse simulate(@Valid @RequestBody SimulationOrder order) {
+        return customerService.simulateOrderPrice(order);
+    }
+
+    // ResponseEntity - 指定返回状态码 - 可以作为一个课后思考题
+    @PostMapping("placeOrder")
+    public ShoppingCart checkout(@Valid @RequestBody ShoppingCart info) {
+        return customerService.placeOrder(info);
+    }
+
+
+    // 实现的时候最好封装一个search object类
+    @PostMapping("findCoupon")
+    public List<CouponInfo> findCoupon(@Valid @RequestBody SearchCoupon request) {
+        return customerService.findCoupon(request);
+    }
+
+}
+
